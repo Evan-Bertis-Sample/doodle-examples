f09bac569a25f5f6adcc691a0dce08d4431646f7
evan-bertis-sample <esample21@gmail.com>
2024-12-30 12:48:00 -0500

    Begin adding prints to debug

--- display/src/app.c
+++ display/src/app.c
@@ -2,10 +2,11 @@
 #include <doodle/core/modules/doodle_renderer.h>
 #include <doodle/core/util/doodle_math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static void display_setup(doodle_app_t *app) {
+    printf("setup\n");
     doodle_module_renderer_t *renderer = (doodle_module_renderer_t *)doodle_app_get_module(app, DOODLE_MODULE_TYPE_RENDERER);
-
     if (renderer) {
         doodle_color_t color = (doodle_color_t){.value = 0xFF0000FF};
         renderer->clear(renderer, color);
@@ -13,8 +14,8 @@ static void display_setup(doodle_app_t *app) {
 }
 
 static void display_loop(doodle_app_t *app) {
+    printf("loop\n");
     doodle_module_renderer_t *renderer = (doodle_module_renderer_t *)doodle_app_get_module(app, DOODLE_MODULE_TYPE_RENDERER);
-
     if (renderer) {
         doodle_color_t color = (doodle_color_t){.value = 0x00FF00FF};
         renderer->draw_pixel(renderer, 320, 240, color);
@@ -26,7 +27,9 @@ static void display_teardown(doodle_app_t *app) {
 }
 
 doodle_app_desc_t doodle_main(int32_t argc, char *argv[]) {
-    void *module_configs[DOODLE_MODULE_TYPE_COUNT] = {0};
+    void **module_configs = calloc(DOODLE_MODULE_TYPE_COUNT, sizeof(void *));
+
+    printf("main\n");
 
     doodle_module_renderer_config_t *renderer_config = malloc(sizeof(doodle_module_renderer_config_t));
     *renderer_config = (doodle_module_renderer_config_t){
@@ -42,5 +45,7 @@ doodle_app_desc_t doodle_main(int32_t argc, char *argv[]) {
         .loop = display_loop,
         .teardown = display_teardown,
     };
+
+    printf("main end\n");
     return desc;
 }
