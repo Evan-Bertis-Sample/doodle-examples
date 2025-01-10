dafcf920acb7a33389019570647076c94327e015
evan-bertis-sample <esample21@gmail.com>
2025-01-10 17:15:39 -0600

    Some updates

--- display/doodle.cmake
+++ display/doodle.cmake
@@ -8,9 +8,6 @@
 function(build_project)
     # link blank.c to PROJECT_NAME executable, which has already been defined
     target_sources(${PROJECT_NAME} PRIVATE ${DOODLE_PROJECT_ROOT_DIR}/src/app.c)
-
     message(STATUS "Linking doodle library")
-    target_link_libraries(${PROJECT_NAME} PRIVATE doodle)
-
     set(CMAKE_BUILD_TYPE Debug) # set the build type to Debug
 endfunction()
--- display/src/app.c
+++ display/src/app.c
@@ -9,7 +9,7 @@ static void display_setup(doodle_app_t *app) {
     DOODLE_APP_LOG("setup\n");
     doodle_module_renderer_t *renderer = (doodle_module_renderer_t *)doodle_app_get_module(app, DOODLE_MODULE_TYPE_RENDERER);
     if (renderer) {
-        doodle_color_t color = (doodle_color_t){.value = 0xFF0000FF};
+        doodle_color_t color = DOODLE_COLOR_BLACK;
         renderer->clear(renderer, color);
     }
 }
@@ -18,7 +18,7 @@ static void display_loop(doodle_app_t *app) {
     // DOODLE_APP_LOG("loop\n");
     doodle_module_renderer_t *renderer = (doodle_module_renderer_t *)doodle_app_get_module(app, DOODLE_MODULE_TYPE_RENDERER);
     if (renderer) {
-        doodle_color_t color = (doodle_color_t){.value = 0x00FF00FF};
+        doodle_color_t color = DOODLE_COLOR_WHITE;
         renderer->draw_pixel(renderer, 320, 240, color);
     }
 }
@@ -27,7 +27,8 @@ static void display_teardown(doodle_app_t *app) {
     DOODLE_APP_LOG("teardown\n");
 }
 
-doodle_app_desc_t doodle_main(int32_t argc, char *argv[]) {
+doodle_app_desc_t doodle_main(int argc, char *argv[]) {
+    DOODLE_APP_LOG("main start\n");
     doodle_app_desc_t desc = {
         .name = "blank",
         .module_configs = {0},
@@ -41,9 +42,9 @@ doodle_app_desc_t doodle_main(int32_t argc, char *argv[]) {
         .width = 640,
         .height = 480,
     };
-    DOODLE_APP_LOG("Renderer config address: %p\n", renderer_config);
-    desc.module_configs[DOODLE_MODULE_TYPE_RENDERER] = renderer_config;
 
+    desc.module_configs[DOODLE_MODULE_TYPE_RENDERER] = renderer_config;
+    
     DOODLE_APP_LOG("main end\n");
     return desc;
 }
